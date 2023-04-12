104c829d8ac87e0e97fd872e739c259c2accc47d
adoniran <adoniranfranca@gmail.com>
2023-04-12 13:23:29 -0300

    adding form and logo in form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,26 @@
 class CompaniesController < ApplicationController
-  def new; end
+  def new 
+    @company = current_user.build_company
+  end
 
-  def create; end
+  def create
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash[:notice] = "Empresa cadastrada com sucesso"
+      redirect_to root_path
+    else
+      flash[:error] = "Erro ao cadastrar a empresa"
+      render :new
+    end
+  end
 
   def edit; end
 
   def update; end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
+  end
 end
