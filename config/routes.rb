6d6bc71aea0bef0b1d604752edd058a3e9b9f49b
adoniran <adoniranfranca@gmail.com>
2023-04-20 21:03:56 -0300

    refactoring links with helpers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :applicants, :companies, :positions
+  get '/vaga/:slug', action: :public_position, controller: :positions, as: :public_position
 end
